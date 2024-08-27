32af50836c1d03ac3357949d627d702705748a38
Harsh Doshi <harshdoshi999@gmail.com>
2024-08-27 18:11:37 +0530

    import new routes to server file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const socialAuthRoutes = require("./auth");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use(socialAuthRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
