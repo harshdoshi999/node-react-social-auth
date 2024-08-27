89923192a6ac147f2cc0e27a7ece7a8eff28eb40
Harsh Doshi <harshdoshi999@gmail.com>
2024-08-27 18:33:09 +0530

    updaed .env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
 );
